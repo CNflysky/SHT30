33b557bc61952e96e73c81a12946cd37b53cf467
CNflysky <cnflysky@qq.com>
2021-04-22 10:09:51 +0800

    removed redundant functions

--- examples/SHT30_temphumi/SHT30_temphumi.ino
+++ examples/SHT30_temphumi/SHT30_temphumi.ino
@@ -1,9 +1,11 @@
 #include <SHT30.h>
 SHT30 sht30(0x45);
 void setup(){
-	Serial.begin(115200);
+	Serial.begin(9600);
 	Serial.println();
 	sht30.begin();
+	sht30.enableCRC(true);
+	Serial.printf("CRC state:%d\n",sht30.enableCRC());
 }
 
 void loop(){
